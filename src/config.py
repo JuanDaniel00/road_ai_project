0260e78f94d09d9f9237499ac5e9ecfe2f65c31a
JuanDaniel00 <juanuribe2828@gmail.com>
2026-02-11 12:36:22 -0500

    Agregar archivos iniciales: .gitignore, README.md, requirements.txt, src/config.py, src/extract_frames.py, src/gps_sync.py, src/init.py, src/main.py

---
+++ src/config.py
@@ -0,0 +1,4 @@
+VIDEO_PATH = "data/raw_video/video.mp4"
+GPS_PATH = "data/gps_logs/gps.csv"
+FRAME_OUTPUT = "output/frames/"
+DISTANCE_INTERVAL = 7  # metros
---
+++ src/main.py
@@ -0,0 +1,6 @@
+import cv2
+import pandas as pd
+import numpy as np
+from haversine import haversine
+
+print("Todo cargó bien.")
